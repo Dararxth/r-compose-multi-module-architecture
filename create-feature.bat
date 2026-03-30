1ba4b64a10efbcccf920b5ee4a85573deb7e6728
Dararxth <trydararoth@gmail.com>
2026-03-30 11:53:35 +0700

    - .\create-feature.bat

--- create-feature.bat
+++ create-feature.bat
@@ -3,11 +3,8 @@
 :: Compose Clean Architecture Feature Generator
 :: =========================================
 
-:: Set colors
-:: 0 = Black, 1 = Blue, 2 = Green, 3 = Aqua, 4 = Red, 5 = Purple, 6 = Yellow, 7 = White, 8 = Gray, 9 = Light Blue
-:: Example: color 0A = black background + green text
-color 0A
-
+:: Enable ANSI escape codes (Windows 10+)
+:: Green = \x1b[32m, Reset = \x1b[0m
 :: Check feature name
 if "%1"=="" (
     set /p FEATURE=Enter feature name:
@@ -19,29 +16,30 @@ if "%1"=="" (
 set BASE=%CD%\app\src\main\java\com\rxth\multimodule\feature\%FEATURE%
 
 :: Header
-echo =========================================
-echo      Feature: %FEATURE%
-echo      Path: %BASE%
-echo =========================================
+echo.
+echo ------------------------------------------
+echo ==========================================
+echo Generating Feature %FEATURE% ...
+echo ==========================================
 echo.
 
 :: List of folders to create
 set FOLDERS=data\remote data\dto data\repository data\mapper ^
 domain\model domain\repository domain\usecase ^
+di\ ^
 presentation\screen presentation\state presentation\viewmodel
 
 :: Loop through folders with color
 for %%F in (%FOLDERS%) do (
     mkdir "%BASE%\%%F" >nul 2>&1
-    :: green text for created
     echo [CREATED] %%F
 )
 
 :: Footer
 echo.
-echo =========================================
-echo      Feature "%FEATURE%" Successfully Created!
-echo =========================================
+echo ==========================================
+echo Feature "%FEATURE%" Created!
+echo ==========================================
 echo.
 
 :: Pause to see result
